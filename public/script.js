50d46f74588cd62f4878aebe0e298627f4af2705
jinw06k <jinuk1024@gmail.com>
2025-01-24 01:00:08 -0500

    testing

--- public/script.js
+++ public/script.js
@@ -4,3 +4,25 @@ function toggleMenu(){
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+const texts = [
+    "Jin Wook Shin",
+    "2nd year in Computer Engineering",
+    "Taking EECS 216,373,445",
+    "VP of KISA",
+    "Listening to J-POP",
+    "EECS 545 Grader",
+    "Into rock-climbing",
+    "Self-studying LLM",
+];
+
+let currentIndex = 0;
+
+const titleElement = document.querySelector(".name-title");
+
+function updateText() {
+    currentIndex = (currentIndex + 1) % texts.length; 
+    titleElement.textContent = texts[currentIndex];
+}
+
+titleElement.addEventListener("click", updateText);
